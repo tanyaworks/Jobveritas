e5251b81a0184040962b486138bb39af128a0f83
tanyajha12 <jha.tanyaa12@gmail.com>
2026-05-24 14:08:50 +0530

    Update API URL to production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "http://127.0.0.1:5001";
+const API = "https://jobveritas.onrender.com";
 
 const GaugeCircle = ({ value }) => {
   const radius = 54;
